b3fe310f15602df26ca368c47464fa1ea03498f1
Gangadhara Inti <Gangadhara.Inti@standard.com>
2020-07-27 12:55:48 -0700

    updates

--- iq_common.py
+++ iq_common.py
@@ -11,6 +11,48 @@ iq_session.verify = 'hlblbclmp001-standard-com-chain.pem'
 iq_session.cookies.set('CLM-CSRF-TOKEN', 'api')
 iq_session.headers.update({'X-CSRF-TOKEN': 'api'})
 
+iq_url = "https://iqserver.standard.com"
+
+orgsl = iq_session.get(f'{iq_url}/api/v2/organizations').json()["organizations"]
+orgs = {}
+for org in orgsl:
+    orgs.update({org["id"]: org["name"]})
+
+orgtagslist = iq_session.get(f'{iq_url}/api/v2/organizations/ROOT_ORGANIZATION_ID').json()["tags"]
+for otl in orgtagslist:
+    if otl["name"] == "Internal":
+        internalTag = otl["id"]
+    if otl["name"] == "Distributed":
+        distributedTag = otl["id"]
+    if otl["name"] == "Remediated":
+        remediatedTag = otl["id"]
+    if otl["name"] == "Hosted":
+        hostedTag = otl["id"]
+
+#apps = iq_session.get(f'{iq_url}/api/v2/applications?publicId=PDM7').json()["applications"]
+apps = iq_session.get(f'{iq_url}/api/v2/applications').json()["applications"]
+
+# Enrich Apps
+for app in apps:
+    app["organization"] = orgs.get(app["organizationId"])
+    app["app_internal"] = ""
+    app["app_Distributed"] = ""
+    app["app_remediated"] = ""
+    app["app_hosted"] = ""
+    
+    for apptag in app["applicationTags"]:
+        if apptag["tagId"] == internalTag:
+            app["app_internal"] = "Inernal"
+        if apptag["tagId"] == distributedTag:
+            app["app_Distributed"] = "Distributed"
+        if apptag["tagId"] == remediatedTag:
+            app["app_remediated"] = "Remediated"
+        if apptag["tagId"] == hostedTag:
+            app["app_hosted"] = "Hosted"
+    app.pop("organizationId")
+    app.pop("applicationTags")
+    app.pop("id")    
+
 def savecsvreport(file_name, csvrecords):
     if not os.path.exists("output"):
         os.mkdir("output")
