6a2a3a6ca42e70559bc23a5624c8d87c4d028ca8
Gangadhara Inti <Gangadhara.Inti@standard.com>
2020-07-27 19:37:31 -0700

    updates

--- iq_policy_violations.py
+++ iq_policy_violations.py
@@ -6,7 +6,7 @@ import csv
 import os
 
 iq_session = requests.Session()
-iq_session.auth = requests.auth.HTTPBasicAuth(getpass.getuser(), getpass.getpass(prompt='Password: ', stream=None))
+iq_session.auth = requests.auth.HTTPBasicAuth(getpass.getuser(), "July2020$") # getpass.getpass(prompt='Password: ', stream=None))
 iq_session.verify = 'hlblbclmp001-standard-com-chain.pem'
 iq_session.cookies.set('CLM-CSRF-TOKEN', 'api')
 iq_headers = {'X-CSRF-TOKEN': 'api'}
@@ -39,75 +39,67 @@ orgtagslist = iq_session.get(f'{iq_url}/api/v2/organizations/ROOT_ORGANIZATION_I
 for otl in orgtagslist:
     if otl["name"] == "Internal":
         internaltag = otl["id"]
-#print("internaltag: " + internaltag)
 
-#apps = iq_session.get(f'{iq_url}/api/v2/applications?publicId=PDM7').json()["applications"]
-apps = iq_session.get(f'{iq_url}/api/v2/applications').json()["applications"]
+apps = iq_session.get(f'{iq_url}/api/v2/applications?publicId=Drupal7').json()["applications"]
+#apps = iq_session.get(f'{iq_url}/api/v2/applications').json()["applications"]
 
 polviolationreport = []
+
 for app in apps:
-    #publicId = app["publicId"]
     app_id = app["id"]
     app["app_internal"] = ""
-    #print(app["publicId"])
     for apptag in app["applicationTags"]:
         if apptag["tagId"] == internaltag:
             app["app_internal"] = "Inernal"
 
     reportIds = iq_session.get(f'{iq_url}/api/v2/reports/applications/{app_id}').json()
     for reportId in reportIds:
-        repStage = reportId["stage"]
+        #repStage = reportId["stage"]
         repUrl   = reportId["reportDataUrl"]
-        repDate  = reportId["evaluationDate"]
 
-        #reportId["appPublicId"] = app["publicId"]
-        #reportId["app_internal"] = app["app_internal"]
-        
-        #print("policyrepurl: "+ rawrepUrl)
         rawrep = iq_session.get(f'{iq_url}/{repUrl}').json() # this is BOM or raw report
-        #print("rawrep :" + str(rawrep))
-        #saveOutput("rawrep_file-"+repStage, rawrep)
         
         policyrepurl = str(repUrl).replace("raw", "policy")
-        #print("policyrepurl: "+ policyrepurl)
         policyrep = iq_session.get(f'{iq_url}/{policyrepurl}').json() # this is Policy violations report
-        #print("policyrep in Stage: "+ repStage +": " + str(policyrep))    
-        #saveOutput("policyrep_file-"+repStage, policyrep)
         polComponents = policyrep["components"]
-        onlyComponents = polComponents.copy() 
-        for onlyComponent in onlyComponents:
+
+        for polComponent in polComponents:
+            for compViolation in polComponent["violations"]:
+                secViolation = {}
+                if compViolation["policyThreatCategory"] != "SECURITY":
+                    compViolation.clear()
+                else:    
+                    secViolation["organization"] = orgs.get(policyrep["application"]["organizationId"])
+                    secViolation["appName"] = policyrep["application"]["name"]
+                    secViolation["apppublicId"] = policyrep["application"]["publicId"]
+                    secViolation["Stage"] = reportId["stage"]
+                    #secViolation["evaluationDate"]  = reportId["evaluationDate"]
+                    #secViolation["reportDataUrl"]  = reportId["reportDataUrl"]
+                    secViolation["hash"] = polComponent["hash"]
+                    secViolation["displayName"] = polComponent["displayName"]
+                    secViolation["packageUrl"] = polComponent["packageUrl"]
+                    secViolation["proprietary"] = polComponent["proprietary"]                                
+                    secViolation["matchState"] = polComponent["matchState"]
+                    secViolation["policyThreatCategory"] = compViolation["policyThreatCategory"]                    
+                    secViolation["policyName"] = compViolation["policyName"]
+                    secViolation["policyThreatLevel"] = compViolation["policyThreatLevel"]
+                    secViolation["CVE"] = ""
+                    if compViolation["policyThreatCategory"] == "SECURITY":
+                        for polCons in compViolation["constraints"]:
+                            for polcond in polCons["conditions"]:
+                                conReason = polcond.get("conditionReason")
+                                i = conReason.find("with")
+                                secViolation["CVE"] = conReason[28:i]
+                    #secViolation["policyViolationId"] = compViolation["policyViolationId"]
+                    secViolation["waived"] = compViolation["waived"]
+                    #secViolation["grandfathered"] = compViolation["grandfathered"]
+                    secViolation["pathnames"] = str(polComponent["pathnames"])[0:1000]
+                    polviolationreport.append(secViolation)
             #onlyComponent.pop("violations")
             #onlyComponent.pop("componentIdentifier")
             #onlyComponent.pop("pathnames")
-            for policyViolation in onlyComponent["violations"]:
-                policyViolation["appName"] = policyrep["application"]["name"]
-                policyViolation["apppublicId"] = policyrep["application"]["publicId"]
-                policyViolation["organization"] = orgs.get(policyrep["application"]["organizationId"])
-                policyViolation["Stage"] = reportId["stage"]
-                policyViolation["evaluationDate"]  = reportId["evaluationDate"]
-                policyViolation["reportDataUrl"]  = reportId["reportDataUrl"]
-                policyViolation["packageUrl"] = onlyComponent["packageUrl"]
-                policyViolation["hash"] = onlyComponent["hash"]
-                policyViolation["proprietary"] = onlyComponent["proprietary"]                                
-                policyViolation["matchState"] = onlyComponent["matchState"]
-                policyViolation["displayName"] = onlyComponent["displayName"]
-                policyViolation["CVE"] = ""
-                if policyViolation["policyThreatCategory"] == "SECURITY":
-                    for polCons in policyViolation["constraints"]:
-                        for polcond in polCons["conditions"]:
-                            conReason = polcond.get("conditionReason")
-                            i = conReason.find("with")
-                            policyViolation["CVE"] = conReason[28:i]
-                policyViolation.pop("constraints")
-                polviolationreport.append(policyViolation)
-
         #print("components in stage: "+repStage+" : "+str(onlyComponents))
         #saveOutput("onlyCompoents_file-"+repStage, onlyComponents)
 
 savecsvreport("PolicyViolations", polviolationreport)
-
-
-#print("reportIds: " + str(reportIds))
-#savecsvreport("Reports-list", reportIds)
-
-#print(apps)
+saveOutput("Policy-Violations", policyrep)
