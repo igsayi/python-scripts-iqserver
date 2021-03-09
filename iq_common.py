9fec1f25017fb416e93c55aa193b05ee9a047508
Gangadhara Sayi inti <gangadhara.inti@standard.com>
2021-03-08 16:14:47 -0800

    changes

--- iq_common.py
+++ iq_common.py
@@ -6,7 +6,8 @@ import csv
 import os
 
 iq_session = requests.Session()
-iq_session.auth = requests.auth.HTTPBasicAuth(getpass.getuser(), getpass.getpass(prompt='Password: ', stream=None))
+uPass = getpass.getpass(prompt='Password: ', stream=None)
+iq_session.auth = requests.auth.HTTPBasicAuth(getpass.getuser(), uPass)
 iq_session.verify = 'hlblbclmp001-standard-com-chain.pem'
 iq_session.cookies.set('CLM-CSRF-TOKEN', 'api')
 iq_session.headers.update({'X-CSRF-TOKEN': 'api'})
@@ -29,8 +30,8 @@ for otl in orgtagslist:
     if otl["name"] == "Hosted":
         hostedTag = otl["id"]
 
-apps = iq_session.get(f'{iq_url}/api/v2/applications?publicId=PDM7').json()["applications"]
-#apps = iq_session.get(f'{iq_url}/api/v2/applications').json()["applications"]
+#apps = iq_session.get(f'{iq_url}/api/v2/applications?publicId=account-linking-service').json()["applications"]
+apps = iq_session.get(f'{iq_url}/api/v2/applications').json()["applications"]
 
 # Enrich Apps
 for app in apps:
@@ -44,19 +45,20 @@ for app in apps:
         if apptag["tagId"] == internalTag:
             app["app_internal"] = "Inernal"
         if apptag["tagId"] == distributedTag:
-            app["app_Distributed"] = "Distributed"
+           app["app_Distributed"] = "Distributed"
         if apptag["tagId"] == remediatedTag:
-            app["app_remediated"] = "Remediated"
+           app["app_remediated"] = "Remediated"
         if apptag["tagId"] == hostedTag:
-            app["app_hosted"] = "Hosted"
+           app["app_hosted"] = "Hosted"
     app.pop("organizationId")
+    app.pop("contactUserName")
     app.pop("applicationTags")
     #app.pop("id")    
 
 def savecsvreport(file_name, csvrecords):
     if not os.path.exists("output"):
         os.mkdir("output")
-    reportfile = f'temp/{file_name}-{format(datetime.now().strftime("%Y%m%d"))}.csv'
+    reportfile = f'output/{file_name}-{format(datetime.now().strftime("%Y%m%d"))}.csv'
     report_data = open(reportfile, 'w', newline='')
     csvwriter = csv.writer(report_data)
     if csvrecords:
@@ -67,6 +69,6 @@ def savecsvreport(file_name, csvrecords):
     report_data.close()
 
 def saveOutput(file_name, d):
-    reportfile = f'temp/{file_name}-{format(datetime.now().strftime("%Y%m%d"))}.json'
+    reportfile = f'output/{file_name}-{format(datetime.now().strftime("%Y%m%d"))}.json'
     fileout = open(reportfile,"w")
     fileout.write(json.dumps(d, indent=4))
