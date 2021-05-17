57fffbef08954740f5bd018d3f793a16664f0c3a
Gangadhara Sayi inti <gangadhara.inti@standard.com>
2021-05-16 19:52:34 -0700

    iq_apps

--- iq_apps.py
+++ iq_apps.py
@@ -1,4 +1,6 @@
 from iq_common import savecsvreport as savecsvreport
+from iq_common import saveOutput as saveOutput
 from iq_common import apps as apps
  
 savecsvreport("appReport", apps)
+saveOutput("appReport", apps)
