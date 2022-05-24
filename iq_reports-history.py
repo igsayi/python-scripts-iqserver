c42547f379937e8debf2b43bd60c5807e88c268a
Gangadhara Sayi inti <gangadhara.inti@standard.com>
2022-05-23 23:34:45 -0700

    refactored

--- iq_reports-history.py
+++ iq_reports-history.py
@@ -1,33 +1,36 @@
+import os.path
+
+from iq_common import apps as apps
 from iq_common import iq_session as iq_session
 from iq_common import savecsvreport as savecsvreport
-from iq_common import apps as apps
 
-iq_url = "https://iqserver.standard.com"
 
-finalReport = []
+def main():
 
-for app in apps:
-    app_id = app["id"]
+    iq_url = "https://iqserver.standard.com"
+    finalReport = []
+    for app in apps:
+        app_id = app["id"]
+        reports = iq_session.get(f'{iq_url}/api/v2/reports/applications/{app_id}/history').json()["reports"]
+        if reports is not None and len(reports) > 0:
+            for report in reports:
+                # if reportId["stage"] != "release":
+                #    continue
+                finalReportrecord = {}
+                finalReportrecord.update(app)
+                finalReportrecord["stage"] = report["stage"]
+                finalReportrecord["evaluationDate"] = report["evaluationDate"]
+                finalReportrecord["reportDataUrl"] = report["reportDataUrl"]
+                finalReportrecord["policyEvaluationId"] = report["policyEvaluationId"]
+                finalReportrecord["scanId"] = report["scanId"]
+                finalReportrecord["isForMonitoring"] = report["isForMonitoring"]
+                finalReportrecord["commitHash"] = report["commitHash"]
+                finalReportrecord["affectedComponentCount"] = report["policyEvaluationResult"]["affectedComponentCount"]
+                finalReportrecord["totalComponentCount"] = report["policyEvaluationResult"]["totalComponentCount"]
+                finalReport.append(dict(finalReportrecord))
 
-    reports = iq_session.get(f'{iq_url}/api/v2/reports/applications/{app_id}/history').json()["reports"]
-    if reports is not None and len(reports) >0:
-        for report in reports:
-            #if reportId["stage"] != "release":
-            #    continue
-            finalReportrecord = {}    
-            finalReportrecord.update(app)
-            finalReportrecord["stage"] = report["stage"]
-            finalReportrecord["evaluationDate"] = report["evaluationDate"]
-            finalReportrecord["reportDataUrl"] = report["reportDataUrl"]
+    savecsvreport(os.path.splitext(os.path.basename(__file__))[0], finalReport)
 
-            finalReportrecord["policyEvaluationId"] = report["policyEvaluationId"]
-            finalReportrecord["scanId"] = report["scanId"]
-            finalReportrecord["isForMonitoring"] = report["isForMonitoring"]
-            finalReportrecord["commitHash"] = report["commitHash"]
-            finalReportrecord["affectedComponentCount"] = report["policyEvaluationResult"]["affectedComponentCount"]
-            finalReportrecord["totalComponentCount"] = report["policyEvaluationResult"]["totalComponentCount"]
 
-            finalReport.append(dict(finalReportrecord))
-
-savecsvreport("iq_reports-history", finalReport)
-
+if __name__ == "__main__":
+    main()
--- iq_reports.py
+++ iq_reports.py
@@ -1,27 +1,30 @@
+import os.path
+
+from iq_common import apps as apps
 from iq_common import iq_session as iq_session
 from iq_common import savecsvreport as savecsvreport
-from iq_common import apps as apps
 
-iq_url = "https://iqserver.standard.com"
 
-finalReport = []
+def main():
 
-for app in apps:
-    app_id = app["id"]
+    iq_url = "https://iqserver.standard.com"
+    finalReport = []
+    for app in apps:
+        app_id = app["id"]
+        reports = iq_session.get(f'{iq_url}/api/v2/reports/applications/{app_id}').json()
+        if reports is not None and len(reports) > 0:
+            for report in reports:
+                # if reportId["stage"] != "release":
+                #    continue
+                finalReportrecord = {}
+                finalReportrecord.update(app)
+                finalReportrecord["stage"] = report["stage"]
+                finalReportrecord["evaluationDate"] = report["evaluationDate"]
+                finalReportrecord["reportDataUrl"] = report["reportDataUrl"]
+                finalReport.append(dict(finalReportrecord))
 
-    reports = iq_session.get(f'{iq_url}/api/v2/reports/applications/{app_id}').json()
-        
-    if reports is not None and len(reports) >0:
-        for report in reports:
-            #if reportId["stage"] != "release":
-            #    continue
-            finalReportrecord = {}    
-            finalReportrecord.update(app)
-            finalReportrecord["stage"] = report["stage"]
-            finalReportrecord["evaluationDate"] = report["evaluationDate"]
-            finalReportrecord["reportDataUrl"] = report["reportDataUrl"]
+    savecsvreport(os.path.splitext(os.path.basename(__file__))[0], finalReport)
 
-            finalReport.append(dict(finalReportrecord))
 
-savecsvreport("iq_reports", finalReport)
-
+if __name__ == "__main__":
+    main()
