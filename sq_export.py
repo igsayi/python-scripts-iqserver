86c608b4266e1719a97d364e9010866810b90dc0
Gangadhara Sayi inti <gangadhara.inti@standard.com>
2022-05-26 11:57:07 -0700

    refactored and formatted with black

--- sq_export.py
+++ sq_export.py
@@ -1,35 +1,19 @@
-import requests
-import getpass
-from datetime import datetime
-import json
 import csv
+import getpass
+import json
 import os
+from datetime import datetime
+
 import urllib3
-urllib3.disable_warnings()
+from requests import Session
+from requests.auth import HTTPBasicAuth
 
-sq_session = requests.Session()
-
-uPass = getpass.getpass(prompt='Password: ', stream=None)
-
-sq_session.auth = requests.auth.HTTPBasicAuth(getpass.getuser(), uPass)
-sq_session.verify = 0 
-
-compName = input('Project Name: ')
-#compName = "Adminease-Parent"
-if len(compName) > 0:
-    print("Searching for: "+ str(compName))
-else:
-    print("Running the Report for all applications")
-
-sq_session.headers.update({'XSRF-TOKEN': 'api'})
-
-iq_url = "https://sonarqube.standard.com"
 
 def savecsvreport(file_name, csvrecords):
     if not os.path.exists("output"):
         os.mkdir("output")
     reportfile = f'output/{file_name}-{format(datetime.now().strftime("%Y%m%d"))}.csv'
-    report_data = open(reportfile, 'w', encoding='utf-8', newline='')
+    report_data = open(reportfile, "w", encoding="utf-8", newline="")
     csvwriter = csv.writer(report_data)
     if csvrecords:
         header = csvrecords[0].keys()
@@ -38,60 +22,86 @@ def savecsvreport(file_name, csvrecords):
         csvwriter.writerow(rep_record.values())
     report_data.close()
 
+
 def saveOutput(file_name, d):
     reportfile = f'output/{file_name}-{format(datetime.now().strftime("%Y%m%d"))}.json'
-    fileout = open(reportfile,"w")
+    fileout = open(reportfile, "w")
     fileout.write(json.dumps(d, indent=4))
     fileout.close
 
-allApps=[]
-for i in range(2):
+
+def main():
+
+    urllib3.disable_warnings()
+
+    sq_session = Session()
+    sq_session.auth = HTTPBasicAuth(getpass.getuser(), getpass.getpass(prompt="Password: ", stream=None))
+    sq_session.verify = False
+    sq_session.headers.update({"XSRF-TOKEN": "api"})
+
+    sq_url = "https://sonarqube.standard.com"
+
+    compName = input("Project Name: ")
+    # compName = "Adminease-Parent"
     if len(compName) > 0:
-        apps = sq_session.get(f'{iq_url}/sonarqube/api/components/search?qualifiers=TRK&ps=400&p={i+1}&q={compName}').json()["components"]
+        print("Searching for: " + str(compName))
     else:
-        apps = sq_session.get(f'{iq_url}/sonarqube/api/components/search?qualifiers=TRK&ps=400&p={i+1}').json()["components"]
-    allApps = allApps + apps
+        print("Running the Report for all applications")
 
-#print("apps: "+ str(apps))
-finalReport = []
-for app in allApps:
-    print("app: "+ app["key"])
-    measures = {}
-    finalReportRecord = {}
-    measures = sq_session.get(f'{iq_url}/sonarqube/api/components/app?component={app["key"]}').json()
-    componentShow = sq_session.get(f'{iq_url}/sonarqube/api/components/show?component={app["key"]}').json()["component"]
-    #print("measures: "+ str(measures))
-    #print("componentShow: "+ str(componentShow))
-    measures.update(measures["measures"])
-    measures.update(componentShow)
-    measures.pop("measures")
-    measures.pop("key")
-    measures.pop("uuid")
-    measures.pop("name")
-    measures.pop("longName")
-    measures.pop("q")
-    measures.pop("fav")
-    measures.pop("canMarkAsFavorite")
-    measures.setdefault("lines","")
-    measures.setdefault("coverage","")
-    measures.setdefault("duplicationDensity","")
-    measures.setdefault("issues","")
-    measures.setdefault("tests","")
-    measures.setdefault("analysisDate","")
-    measures.setdefault("version","")
-    finalReportRecord["project"] = measures["project"]
-    finalReportRecord["projectName"] = measures["projectName"]
-    finalReportRecord["lines"] = measures["lines"]
-    finalReportRecord["coverage"] = measures["coverage"]
-    finalReportRecord["duplicationDensity"] = measures["duplicationDensity"]
-    finalReportRecord["issues"] = measures["issues"]
-    finalReportRecord["tests"] = measures["tests"]
-    finalReportRecord["analysisDate"] = measures["analysisDate"]
-    finalReportRecord["version"] = measures["version"]
+    allApps = []
+    for i in range(2):
+        if len(compName) > 0:
+            apps = sq_session.get(f"{sq_url}/sonarqube/api/components/search?qualifiers=TRK&ps=400&p={i+1}&q={compName}").json()[
+                "components"
+            ]
+        else:
+            apps = sq_session.get(f"{sq_url}/sonarqube/api/components/search?qualifiers=TRK&ps=400&p={i+1}").json()["components"]
+        allApps = allApps + apps
 
-    #print("measures: "+ str(measures))
-    #finalReport.append(measures)
-    finalReport.append(finalReportRecord)
-savecsvreport("sq_Report"+compName, finalReport)
-#saveOutput("sq_Report", finalReport)
-#saveOutput("sq_Report-measures", measures)
+    # print("apps: "+ str(apps))
+    finalReport = []
+    for app in allApps:
+        print("app: " + app["key"])
+        measures = {}
+        finalReportRecord = {}
+        measures = sq_session.get(f'{sq_url}/sonarqube/api/components/app?component={app["key"]}').json()
+        componentShow = sq_session.get(f'{sq_url}/sonarqube/api/components/show?component={app["key"]}').json()["component"]
+        # print("measures: "+ str(measures))
+        # print("componentShow: "+ str(componentShow))
+        measures.update(measures["measures"])
+        measures.update(componentShow)
+        measures.pop("measures")
+        measures.pop("key")
+        measures.pop("uuid")
+        measures.pop("name")
+        measures.pop("longName")
+        measures.pop("q")
+        measures.pop("fav")
+        measures.pop("canMarkAsFavorite")
+        measures.setdefault("lines", "")
+        measures.setdefault("coverage", "")
+        measures.setdefault("duplicationDensity", "")
+        measures.setdefault("issues", "")
+        measures.setdefault("tests", "")
+        measures.setdefault("analysisDate", "")
+        measures.setdefault("version", "")
+        finalReportRecord["project"] = measures["project"]
+        finalReportRecord["projectName"] = measures["projectName"]
+        finalReportRecord["lines"] = measures["lines"]
+        finalReportRecord["coverage"] = measures["coverage"]
+        finalReportRecord["duplicationDensity"] = measures["duplicationDensity"]
+        finalReportRecord["issues"] = measures["issues"]
+        finalReportRecord["tests"] = measures["tests"]
+        finalReportRecord["analysisDate"] = measures["analysisDate"]
+        finalReportRecord["version"] = measures["version"]
+
+        # print("measures: "+ str(measures))
+        # finalReport.append(measures)
+        finalReport.append(finalReportRecord)
+    savecsvreport("sq_Report" + compName, finalReport)
+    # saveOutput("sq_Report", finalReport)
+    # saveOutput("sq_Report-measures", measures)
+
+
+if __name__ == "__main__":
+    main()
