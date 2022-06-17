6a1a5d864a01e59726d53ebc3a5fce2964c132ff
Gangadhara Sayi inti <gangadhara.inti@standard.com>
2022-06-16 20:18:51 -0700

    har file

---
+++ One-time/har-ro-csv.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import json
+
+import pandas as pd
+
+datafile = "output/portalacc.standard.com.har"
+filein = open(datafile, encoding="utf8")
+har_json = json.load(filein)
+filein.close
+
+interested_headers = {
+    "ACCESS-CONTROL-ALLOW-ORIGIN",
+    "CACHE-CONTROL",
+    "CLEAR-SITE-DATA",
+    "CONTENT-DISPOSITION",
+    "CONTENT-SECURITY-POLICY",
+    "CONTENT-TYPE",
+    "CROSS-ORIGIN-EMBEDDER-POLICY",
+    "CROSS-ORIGIN-OPENER-POLICY",
+    "CROSS-ORIGIN-RESOURCE-POLICY",
+    "EXPECT-CT",
+    "PERMISSIONS-POLICY",
+    "REFERRER-POLICY",
+    "STRICT-TRANSPORT-SECURITY",
+    "X-CONTENT-TYPE-OPTIONS",
+    "X-FRAME-OPTIONS",
+    "X-XSS-PROTECTION",
+    "EXPIRES",
+    "PRAGMA",
+    "SET-COOKIE",
+}
+
+final_report = []
+for har_entry in har_json["log"]["entries"]:
+    report_record = {}
+    report_record["url_name"] = har_entry["request"]["url"]
+
+    for har_header in har_entry["response"]["headers"]:
+        if har_header["name"].upper() in interested_headers:
+            report_record[har_header["name"].upper()] = har_header["value"]
+    final_report.append(report_record)
+
+df = pd.DataFrame(final_report)
+df.to_csv("output/portalacc.standard.com.har.csv", index=False)
