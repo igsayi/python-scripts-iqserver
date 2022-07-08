3a5cefdea9ebe9a4e06ff2fba1d9842993a952a3
Gangadhara Sayi Inti <igsayi@yahoo.com>
2022-07-07 19:35:20 -0700

    updated

--- One-time/webhook_listener.py
+++ One-time/webhook_listener.py
@@ -1,15 +1,25 @@
+import json
+import os
+from datetime import datetime
+
 from flask import Flask, request  # , Response
 
 app = Flask(__name__)
 
 # Run the Flask application as below
-# export FLASK_APP=main.py
+# export FLASK_APP=webhook_listener.py
 # python -m flask run
 
 
 @app.route("/webhook", methods=["POST"])
 def respond():
     print(request.json)
+    if not os.path.exists("output"):
+        os.mkdir("output")
+    reportfile = f'output/Webhook-{format(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))}.json'
+    fileout = open(reportfile, "w")
+    fileout.write(json.dumps(request.json, indent=4))
+    fileout.close
     return "Webhooks POST with Python was successful "  # Response(status=200)
 
 
