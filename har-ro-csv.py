a85ec1fcf3f1d291edf507a99dad332d3aa0ec4e
Gangadhara Sayi inti <gangadhara.inti@standard.com>
2022-07-20 13:06:04 -0700

    process all the file in a folder

--- har-ro-csv.py
+++ har-ro-csv.py
@@ -1,47 +1,52 @@
 #!/usr/bin/env python
 import json
+import os
 
 import pandas as pd
 
-datafile = "output/portalacc.standard.com.har"
-filein = open(datafile, encoding="utf8")
-har_json = json.load(filein)
-filein.close
+# datafile = "output/portalacc.standard.com.har"
 
-interested_headers = {
-    "ACCESS-CONTROL-ALLOW-ORIGIN",
-    "CACHE-CONTROL",
-    "CLEAR-SITE-DATA",
-    "CONTENT-DISPOSITION",
-    "CONTENT-SECURITY-POLICY",
-    "CONTENT-TYPE",
-    "CROSS-ORIGIN-EMBEDDER-POLICY",
-    "CROSS-ORIGIN-OPENER-POLICY",
-    "CROSS-ORIGIN-RESOURCE-POLICY",
-    "EXPECT-CT",
-    "PERMISSIONS-POLICY",
-    "REFERRER-POLICY",
-    "STRICT-TRANSPORT-SECURITY",
-    "X-CONTENT-TYPE-OPTIONS",
-    "X-FRAME-OPTIONS",
-    "X-XSS-PROTECTION",
-    "EXPIRES",
-    "PRAGMA",
-    "SET-COOKIE",
-}
+os.chdir("C:\Temp\Downloads\Gold-ui-har file")
+directory_list = os.listdir()
+for datafile in directory_list:
+    filein = open(datafile, encoding="utf8")
+    har_json = json.load(filein)
+    filein.close
 
-final_report = []
-for har_entry in har_json["log"]["entries"]:
-    report_record = {}
+    interested_headers = {
+        "ACCESS-CONTROL-ALLOW-ORIGIN",
+        "CACHE-CONTROL",
+        "CLEAR-SITE-DATA",
+        "CONTENT-DISPOSITION",
+        "CONTENT-SECURITY-POLICY",
+        "CONTENT-TYPE",
+        "CROSS-ORIGIN-EMBEDDER-POLICY",
+        "CROSS-ORIGIN-OPENER-POLICY",
+        "CROSS-ORIGIN-RESOURCE-POLICY",
+        "EXPECT-CT",
+        "PERMISSIONS-POLICY",
+        "REFERRER-POLICY",
+        "STRICT-TRANSPORT-SECURITY",
+        "X-CONTENT-TYPE-OPTIONS",
+        "X-FRAME-OPTIONS",
+        "X-XSS-PROTECTION",
+        "EXPIRES",
+        "PRAGMA",
+        "SET-COOKIE",
+    }
 
-    report_record["url_name"] = har_entry["request"]["url"]
-    report_record["resource_type"] = har_entry["_resourceType"]
-    report_record["priority"] = har_entry["_priority"]
+    final_report = []
+    for har_entry in har_json["log"]["entries"]:
+        report_record = {}
 
-    for har_header in har_entry["response"]["headers"]:
-        if har_header["name"].upper() in interested_headers:
-            report_record[har_header["name"].upper()] = har_header["value"]
-    final_report.append(report_record)
+        report_record["url_name"] = har_entry["request"]["url"]
+        report_record["resource_type"] = har_entry["_resourceType"]
+        report_record["priority"] = har_entry["_priority"]
 
-df = pd.DataFrame(final_report)
-df.to_csv("output/portalacc.standard.com.har.csv", index=False)
+        for har_header in har_entry["response"]["headers"]:
+            if har_header["name"].upper() in interested_headers:
+                report_record[har_header["name"].upper()] = har_header["value"]
+        final_report.append(report_record)
+
+    df = pd.DataFrame(final_report)
+    df.to_csv(f"{datafile}.csv", index=False)
