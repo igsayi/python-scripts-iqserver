ad3762e5f8a61da066709afdf6e189d27dd5d7d8
Gangadhara Sayi inti <gangadhara.inti@standard.com>
2022-08-23 13:45:44 -0700

    updates

--- har-ro-csv.py
+++ har-ro-csv.py
@@ -6,9 +6,11 @@ import pandas as pd
 
 # datafile = "output/portalacc.standard.com.har"
 
-os.chdir("C:\Temp\Downloads\Gold-ui-har file")
+os.chdir("C:\Temp\Downloads\har")
 directory_list = os.listdir()
 for datafile in directory_list:
+    if not datafile.endswith(".har"):
+        continue
     filein = open(datafile, encoding="utf8")
     har_json = json.load(filein)
     filein.close
@@ -42,11 +44,19 @@ for datafile in directory_list:
         report_record["url_name"] = har_entry["request"]["url"]
         report_record["resource_type"] = har_entry["_resourceType"]
         report_record["priority"] = har_entry["_priority"]
+        if "connection" in har_entry:
+            report_record["connection"] = har_entry["connection"]
+        else:
+            report_record["connection"] = ""
+        report_record["serverIPAddress"] = har_entry["serverIPAddress"]
 
         for har_header in har_entry["response"]["headers"]:
             if har_header["name"].upper() in interested_headers:
-                report_record[har_header["name"].upper()] = har_header["value"]
+                if har_header["name"].upper() in report_record:
+                    report_record[har_header["name"].upper()] = report_record[har_header["name"].upper()] + "\n" + har_header["value"]
+                else:
+                    report_record[har_header["name"].upper()] = har_header["value"]
         final_report.append(report_record)
 
     df = pd.DataFrame(final_report)
-    df.to_csv(f"{datafile}.csv", index=False)
+    df.to_excel((f"{datafile}.xlsx"), index=False)
