02bd7bc8aa77452ccb20a0ee130e48e09700b67c
Gangadhara Sayi inti <gangadhara.inti@standard.com>
2023-04-14 12:31:59 -0700

    minor change

--- iq_backup.sh
+++ iq_backup.sh
@@ -2,6 +2,7 @@
 cd /c/git/igsayi/python-scripts-iqserver
 python iq_apps.py
 python iq_components.py
+python iq_components-Release-stage.py
 python iq_licensedata.py
 python iq_reports-history.py
 python iq_reports.py
