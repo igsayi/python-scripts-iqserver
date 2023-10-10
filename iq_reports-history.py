70b3c24b0e523c5b1ade7d1fe5e237c1e7f3cb11
Gangadhara Sayi inti <Gangadhara.Inti@standard.com>
2023-10-09 20:08:39 -0700

    change output to excel

--- iq_reports-history.py
+++ iq_reports-history.py
@@ -3,16 +3,15 @@ import os.path
 
 from iq_common import apps as apps
 from iq_common import iq_session as iq_session
-from iq_common import savecsvreport as savecsvreport
+from iq_common import saveExcelReport as saveExcelReport
 
 
 def main():
-
     iq_url = "https://iqserver.standard.com"
     finalReport = []
     for app in apps:
         app_id = app["id"]
-        reports = iq_session.get(f'{iq_url}/api/v2/reports/applications/{app_id}/history').json()["reports"]
+        reports = iq_session.get(f"{iq_url}/api/v2/reports/applications/{app_id}/history").json()["reports"]
         if reports is not None and len(reports) > 0:
             for report in reports:
                 # if reportId["stage"] != "release":
@@ -30,7 +29,7 @@ def main():
                 finalReportrecord["totalComponentCount"] = report["policyEvaluationResult"]["totalComponentCount"]
                 finalReport.append(dict(finalReportrecord))
 
-    savecsvreport(os.path.splitext(os.path.basename(__file__))[0], finalReport)
+    saveExcelReport(os.path.splitext(os.path.basename(__file__))[0], finalReport)
 
 
 if __name__ == "__main__":
