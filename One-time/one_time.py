340f85e05d1373265c018945acc339bc3d46c1be
ginti <ginti@standard.com>
2024-04-14 23:43:11 -0700

    changes

--- One-time/one_time.py
+++ One-time/one_time.py
@@ -7,7 +7,7 @@ pyautogui.FAILSAFE = False
 prevMouseX, prevMouseY = pyautogui.position()
 # print(time.localtime().tm_sec, "First position: ", pyautogui.position())
 while True:
-    time.sleep(500)
+    time.sleep(50)
     currentMouseX, currentMouseY = pyautogui.position()
     # print("Curr vs Prev", currentMouseX, prevMouseX)
     if currentMouseX == prevMouseX:
@@ -18,7 +18,7 @@ while True:
             prevMouseX, prevMouseY = pyautogui.position()
             # print(time.localtime().tm_sec, "position 1: ", i, pyautogui.position())
             break
-        pyautogui.moveTo(100, i * 5)
+        pyautogui.moveTo(100, i * 2)
         # print(time.localtime().tm_sec, "position 2: ", i, pyautogui.position())
         prevMouseX, prevMouseY = pyautogui.position()
     for i in range(0, 3):
